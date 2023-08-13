27ddf550ab238fc207be7a9320d80d1a4434688b
elmø <anon_fwd@icloud.com>
2023-08-13 23:41:56 +0200

    added dependencies

--- linux/server-setup.sh
+++ linux/server-setup.sh
@@ -1,6 +1,6 @@
 if [[ -f /etc/redhat-release ]]; then
   BASIC="git curl neovim util-linux-user ncdu exa bat fd-find tldr"
-  DEV="golang python3 python3-virtualenvwrapper"
+  DEV="golang python3 python3-virtualenvwrapper gcc clang"
   CONTAINER="podman podman-compose slirp4netns fuse-overlayfs"
   yum update -y
   yum install -y $BASIC $DEV $CONTAINER
