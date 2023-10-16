1fda584aaa5271a4a9b2d1dfbe8bdfa394f0d9dd
elmø <anon_fwd@icloud.com>
2023-10-16 21:36:16 +0200

    update utilities

--- linux/server-setup.sh
+++ linux/server-setup.sh
@@ -8,7 +8,7 @@ fi
 
 if [[ -f /etc/lsb-release ]]; then
   apt-get update -y
-  BASIC="git curl neovim ncdu exa bat fd-find tldr"
+  BASIC="git curl neovim ncdu lsd bat fd-find tldr"
   DEV="golang python3 python3-virtualenvwrapper gcc clang"
   apt install -y $BASIC $DEV
 fi
