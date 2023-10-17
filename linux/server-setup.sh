cd6da7f816ae7f51a5012577c372a8d70ba88372
elmø <anon_fwd@icloud.com>
2023-10-17 21:33:30 +0200

    update: topgrade instructions + fix: server-setup on ubuntu

--- linux/server-setup.sh
+++ linux/server-setup.sh
@@ -8,8 +8,7 @@ fi
 
 if [[ -f /etc/lsb-release ]]; then
   apt-get update -y
-  BASIC="git curl neovim ncdu bat fd-find tldr"
+  BASIC="git curl neovim ncdu bat fd-find tldr lsd"
   DEV="golang python3 python3-virtualenvwrapper gcc clang"
   apt install -y $BASIC $DEV
-  snap install lsd
 fi
--- linux/topgrade.sh
+++ linux/topgrade.sh
@@ -6,8 +6,10 @@ if [[ -f /etc/redhat-release ]]; then
 fi
 
 if [[ -f /etc/lsb-release ]]; then
-  apt-get update -y
-  apt-get install -y cargo libssl-dev pkg-config
+  apt update -y
+  apt remove -y rust
+  curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+  apt  install -y cargo libssl-dev pkg-config
 fi
 
 
