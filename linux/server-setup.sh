f9b3c8467441db1a07ba2950fd0ce230d9159675
elmø <anon_fwd@icloud.com>
2023-10-19 20:32:19 +0200

    added zsh in server setup

--- linux/server-setup.sh
+++ linux/server-setup.sh
@@ -1,5 +1,5 @@
 if [[ -f /etc/redhat-release ]]; then
-  BASIC="git curl neovim util-linux-user ncdu exa bat fd-find tldr"
+  BASIC="git curl neovim util-linux-user ncdu exa bat fd-find tldr zsh"
   DEV="golang python3 python3-virtualenvwrapper gcc clang"
   CONTAINER="podman podman-compose slirp4netns fuse-overlayfs"
   yum update -y
@@ -8,7 +8,7 @@ fi
 
 if [[ -f /etc/lsb-release ]]; then
   apt-get update -y
-  BASIC="git curl neovim ncdu bat fd-find tldr"
+  BASIC="git curl neovim ncdu bat fd-find tldr zsh"
   DEV="golang python3 python3-virtualenvwrapper gcc clang"
   apt install -y $BASIC $DEV
 fi
